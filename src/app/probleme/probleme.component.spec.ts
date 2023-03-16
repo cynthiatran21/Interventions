a86e393d195dec5153d289504a266c4107735611
CYNTHIA TRAN <yencynthia.tran@gmail.com>
2023-03-16 13:12:51 -0400

    #1 | Zone PRÉNOM invalide avec 2 caractères

---
+++ src/app/probleme/probleme.component.spec.ts
@@ -0,0 +1,32 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { ProblemeComponent } from './probleme.component';
+
+describe('ProblemeComponent', () => {
+  let component: ProblemeComponent;
+  let fixture: ComponentFixture<ProblemeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule],
+      declarations: [ ProblemeComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ProblemeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+   it('#1 | Zone PRÉNOM invalide avec 2 caractères', () => {
+    let saisiePrenom = component.problemeForm.controls['prenom'];
+    saisiePrenom.setValue('a'.repeat(3
+      ))
+    expect(saisiePrenom.valid).toBeTruthy();
+   })
+});
