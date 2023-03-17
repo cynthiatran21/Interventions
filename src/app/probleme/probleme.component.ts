c548a5a899bc94be8e1c217f286eabb776c8405b
CYNTHIA TRAN <yencynthia.tran@gmail.com>
2023-03-17 16:13:41 -0400

    TP8 complété

--- src/app/probleme/probleme.component.ts
+++ src/app/probleme/probleme.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { VerifierCaracteresValidator } from '../shared/validerzones/longueur-minimum.component';
 
 @Component({
   selector: 'Inter-probleme',
@@ -13,7 +14,7 @@ export class ProblemeComponent implements OnInit {
 
   ngOnInit() {
     this.problemeForm = this.fb.group({
-      prenom: ['', [Validators.required, Validators.minLength(3)]]
+      prenom: ['' , [VerifierCaracteresValidator.longueurMinimum(3), Validators.required]],
     });
   }
 
